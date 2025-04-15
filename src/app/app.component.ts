d272cc637415ca1dd109603249f06d41516674ee
Ilhami Kandemir <ilhami.1424@gmail.com>
2025-04-15 17:42:42 +0200

    Update: Fonctionnement des routeurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent,NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
