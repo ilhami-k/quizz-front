1105ce07baa8e666e47b859dbfd29941487ff99c
Skrinja <clement.simon@hainaut-promsoc.be>
2025-06-07 23:42:06 +0200

    UPDATE : Modif pour avoir les bonnes catégories
    Affichage des bonnes catégories sur l'accueil
    ATTENTION pas d'icon et de description (Mettre en DB???)

---
+++ src/app/quiz-by-category/quiz-by-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-by-category',
+  imports: [],
+  templateUrl: './quiz-by-category.component.html',
+  styleUrl: './quiz-by-category.component.css'
+})
+export class QuizByCategoryComponent {
+
+}
