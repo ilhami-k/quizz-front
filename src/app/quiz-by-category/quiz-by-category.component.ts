3c8f5abf5f97c2b3b6d46bd3f356b819a0807a6e
Skrinja <clement.simon@hainaut-promsoc.be>
2025-06-07 23:46:43 +0200

    suite en construction

--- src/app/quiz-by-category/quiz-by-category.component.ts
+++ src/app/quiz-by-category/quiz-by-category.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { EnConstructionComponent } from "../en-construction/en-construction.component";
 
 @Component({
   selector: 'app-quiz-by-category',
-  imports: [],
+  imports: [EnConstructionComponent],
   templateUrl: './quiz-by-category.component.html',
   styleUrl: './quiz-by-category.component.css'
 })
