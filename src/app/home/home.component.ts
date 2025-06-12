6da3c2c6e29e0d2468a1b9bd690240e99eee244a
Ilhami Kandemir <ilhami.1424@gmail.com>
2025-06-12 18:53:20 +0200

    style changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CategoriesComponent } from '../categories/categories.component';
 import { ConnexionComponent } from '../connexion/connexion.component';
 import { CommonModule } from '@angular/common';
 import { UIService } from '../services/ui.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CategoriesComponent, ConnexionComponent, CommonModule],
+    imports: [CategoriesComponent, ConnexionComponent, CommonModule,RouterLink],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
